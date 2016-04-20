bcd2cad7d72448aad07a9d474aab3913b648f608
Niels van Zon <nielsirvin@gmail.com>
2016-04-20 15:58:12 +0200

    updaten jenkins script

--- jenkins.bash
+++ jenkins.bash
@@ -17,9 +17,12 @@ git fetch --all
 echo "Checking out target branch $TARGET_BRANCH..." 
 git checkout $TARGET_BRANCH
 git pull
-echo "Checking out base branch $TARGET_BRANCH..." 
+git reset --hard origin/$TARGET_BRANCH
+
+echo "Checking out base branch $BASE_BRANCH..." 
 git checkout $BASE_BRANCH
 git pull
+git reset --hard origin/$BASE_BRANCH
 
 # searches within the commit difference between the base and target branches the commits with #skipMerge in their commit message
 SKIPMERGES=($(git log --reverse --pretty=format:"%H" -i --grep="#skipMerge" $BASE_BRANCH..$TARGET_BRANCH))
@@ -36,7 +39,7 @@ for i in ${SKIPMERGES[@]}; do
 
 	# do a default merge with the before skip commit
 	echo "Merging commit before skip '$BEFORE_SKIP_COMMIT' into '$BASE_BRANCH' with 'default' strategy..."
-	git merge $BEFORE_SKIP_COMMIT -m "Merge '$BEFORE_SKIP_COMMIT' into '$BASE_BRANCH'"
+	git merge $BEFORE_SKIP_COMMIT --no-edit -m "Merge '$BEFORE_SKIP_COMMIT' into '$BASE_BRANCH'"
 
 	# check for merge conflicts
 	if [ -z "$(git status --porcelain)" ]; then 
@@ -49,7 +52,7 @@ for i in ${SKIPMERGES[@]}; do
 
 	# do an ours merge to skip changes with the skip commit
 	echo "Merging skip commit '$SKIP_COMMIT' into '$BASE_BRANCH' with 'ours' strategy..."
-	git merge $SKIP_COMMIT -s ours -m "Merge '$SKIP_COMMIT' into '$BASE_BRANCH' without changes"
+	git merge $SKIP_COMMIT -s ours --no-edit -m "Merge '$SKIP_COMMIT' into '$BASE_BRANCH' without changes"
 
 done
 
@@ -83,5 +86,6 @@ if [ "$MERGE_CONFLICT_COMMIT" == "" ]; then
 else
 	# report on the failed commit
 	echo "Merging failed on commit '$MERGE_CONFLICT_COMMIT', exiting..."
+    git merge --abort
 	exit 1
 fi
